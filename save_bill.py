3f137cc15191bf4bffa09af6c6f31c61d2e0bf76
man27382210 <man27382210@gmail.com>
2015-03-11 14:35:46 +0800

    remove .DS_Store

--- save_bill.py
+++ save_bill.py
@@ -17,25 +17,21 @@ import os
 	# 	self.url_ct = url_ct
 	# 	self.url_c = url_c
 def saveToMongo(dictUse):
-	print dictUse
 	client = MongoClient('mongodb://localhost:27017/')
 	db = client['councilor']
-	collection = db['bills']
+	collection = db['billAll']
 	id_save = collection.insert(dictUse)
-	print id_save
+	print "save"
 
 
 
 def parseFromUrl(url):
 	r = requests.get(url)
 	data = r.json()
-	next = ""
-	if data.has_key("next"):
-		next = data["next"]
-		results = data["results"]
+	results = data["results"]
 	for result in results:
-		if result["county"] == u'臺北市' and result["election_year"] == '2010':
-			saveToMongo(result)
+		saveToMongo(result)
+		# if result["county"] == u'臺北市' and result["election_year"] == '2010':
 		# 			print term["party"]
 		# 			print term["title"]
 		# 			print term["name"]
@@ -55,15 +51,15 @@ def parseFromUrl(url):
 
 if __name__ == '__main__':
 	# parseFromUrl('http://councils.g0v.tw/api/bills/?format=json')
-	i = 2107
-	while i < 2174:
-		# sleep(3)
-		print i
+	i = 1
+	while i < 2200:
+		sleep(3)
 		try:
+			print('http://councils.g0v.tw/api/bills/?page='+str(i)+'&format=json')
 			parseFromUrl('http://councils.g0v.tw/api/bills/?page='+str(i)+'&format=json')
 		except Exception, e:
 			print e
-			print "can't read"
+			print "can't read" + str(i)
 			if os.path.isfile("/Users/man27382210/Desktop/g0v_paper/no_save.txt"):
 				print "append"
 				with open("no_save.txt", "a") as myfile:
@@ -72,5 +68,4 @@ if __name__ == '__main__':
 				print "new"
 				with open("no_save.txt", "w") as myfile:
 					myfile.write(''+str(i)+'\n')
-			
 		i = i+1
